70ae10c3b596e249220368bb81642c1a3fa27be5
dimpals18 <dimpal.1809@gmail.com>
2026-07-01 20:43:08 +0530

    Bulk sync: 2 submissions

---
+++ Data Structures & Algorithms/concatenation-of-array/submission-0.py
@@ -0,0 +1,10 @@
+class Solution:
+    def getConcatenation(self, nums: List[int]) -> List[int]:
+       ans=[]
+       return nums+nums
+
+       nums=[12,24,36,48]
+       ans=getconcatenation(nums)
+       print(ans)
+
+        
